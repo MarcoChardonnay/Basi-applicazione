8e8809b2d6614f49c5b979d8be8d03d73d076f92
Marco Cardone <marco.cardone2@studio.unibo.it>
2024-06-24 18:12:21 +0200

    .

--- database.php
+++ database.php
@@ -9,5 +9,27 @@ class DatabaseHelper{
             die("Connection failed: " . $this->db->connect_error);
         }
     }
+
+    //Query lato Fornitore
+    //Inserimento di una nuova categoria di prodotti; new category (IDcategory, category_name)
+    public function insertNewCategory($IDcategory, $category_name){
+        $sql = "INSERT INTO category (IDcategory, category_name) VALUES (?, ?)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("is", $IDcategory, $category_name);
+        $stmt->execute();
+        $stmt->close();
+        //check if the query was successful
+        if($stmt->affected_rows == 1) return true;
+        else return false;
+    }
+
+    //Inserimento di una nuova collezione di prodotti; new collection
+    //Inserimento di un nuovo prodotto; new product
+
+    //Query lato Manager
+
+    //Query lato Worker (makeup artist e commesso)
+
+    //Query lato Cliente Registrato
 }
 ?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <h1>Welcome to the index page!
+        <section>From here you can procede to the following sub sites:
+            <ul>
+                <li><a href="#">Link 1</a><br></li>
+                <li><a href="#">Link 2</a><br></li>
+                <li><a href="#">Link 3</a><br></li>
+                <li><a href="#">Link 4</a><br></li>
+            </ul>
+        </section>
+    </h1>
+</body>
+</html>
